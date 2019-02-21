e342a740852ace5a83342b376865c8057e9067e7
jebcjebc <B10521139@gemail.yuntech.edu.tw>
2019-02-21 18:59:28 +0800

    GIANT2

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-studentA = 'GIANT GIANT GIANT.'
-print(studentA)
+mathScores = [60, 70, 10, 20, 81, 63, 1521]
+print(sum(mathScores))
+print(max(mathScores))
+s = sum(mathScores)
+l = len(mathScores)
+print(s/l)
+mathScores.append(63)
+del mathScores[0:2]
+print(mathScores)
+mathScores[2] = 100
+print(mathScores)
+print(100 in mathScores)
+
+ls = [[1, 2, 3],[4, 5, 6]]
+print(ls[0][0])
+print(sorted(mathScores))
